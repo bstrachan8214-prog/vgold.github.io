1bd2de766266082859a5c602f640a964d55d43f7
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2026-02-07 21:13:39 -0800

    Update uv.config.js

--- data/local/pages/unlocker/ultraviolet/uv/uv.config.js
+++ data/local/pages/unlocker/ultraviolet/uv/uv.config.js
@@ -3,10 +3,8 @@ self.__uv$config = {
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    // Absolute paths to the deep folder
     handler: '/data/local/pages/unlocker/ultraviolet/uv.handler.js',
     bundle: '/data/local/pages/unlocker/ultraviolet/uv.bundle.js',
     config: '/data/local/pages/unlocker/ultraviolet/uv.config.js',
-    // Point to the bridge file you just fixed in the root
     sw: '/uv-sw.js' 
 };
