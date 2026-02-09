9e951b7a759f1aa1cd087a91043bcb073c3b4165
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2026-02-08 18:34:03 -0800

    Delete uv-sw.js

--- uv-sw.js
+++
@@ -1,9 +0,0 @@
-importScripts('/data/local/pages/unlocker/ultraviolet/uv/uv.bundle.js');
-importScripts('/data/local/pages/unlocker/ultraviolet/uv/uv.config.js');
-importScripts('/data/local/pages/unlocker/ultraviolet/uv/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(sw.fetch(event));
-});
